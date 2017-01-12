3c4856db8faeeace02d258a2be453a8549991c5c
Uma91 <umas2091@gmail.com>
2017-01-12 18:01:55 +0530

    summary
    destination

---
+++ JallikattuProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JallikattuProject
+//
+//  Created by APPLE on 1/12/17.
+//  Copyright © 2017 APPLE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //Uma
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
